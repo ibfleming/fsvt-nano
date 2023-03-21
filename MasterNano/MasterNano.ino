51b3038c6f638d17fa2877e62ac3f746c88c096d
Ian Fleming <ianfleming678@gmail.com>
2023-03-20 20:34:59 -0700

    More Stuff!

--- MasterNano/MasterNano.ino
+++ MasterNano/MasterNano.ino
@@ -14,6 +14,7 @@
 #include <Wire.h>
 
 #define DEBUG true  // Debugging mode!
+#define RATE 1000U   // Refresh Rate.
 
 // PINS
 #define rxPin 18        // D18 (A4), Receive Pin (RX) for I2C buss
@@ -37,13 +38,13 @@ float avgV = 0, tdsVal = 0, temperature = 25.0;                 // Voltage, TDS,
 
 SoftwareSerial BTSerial(rxPin, txPin);                  // Open Software Serial between Nano and HM-10 BT module on I2C lane.
 static unsigned long programClock = millis();           // Timer/Clock of entire program, in milliseconds.
-unsigned long timeCount = 1000;                              // Counter for a single second that has passed. (1000ms = 1s)
+float timeCount = 0;                              // Counter for a single second that has passed. (1000ms = 1s)
 // millis() => Returns the number of milliseconds passed since the Arduino board began running the current program
 
 // DEFINITIONS
 int getMedianNum(int bArray[], int iFilterLen);
 float calculateTDS(float cVal);
-void fetchTDS(TDSData * TDS);
+void fetchTDS();
 
 /*  ==================================================
     SETUP
@@ -58,32 +59,9 @@ void setup() {
     ================================================== */
 void loop() {
   TDSData * TDS = new TDSData;
-  // TDS->tdsValue = -1;
-  // TDS->voltage = -1;
-  fetchTDS(TDS);  // Get the TDS value and average voltage of that reading. Approximately 840ms (or a multiple) has to pass until a value is actually received, else equal 0.
-  unsigned long tdsClockIteration = millis() - programClock;   // This value represents the amount of time that this function finished executing.
 
-  if( DEBUG ) {
-    if( TDS->tdsValue != -1 && TDS->voltage != -1 ) {
-      Serial.print("(");
-      Serial.print(tdsClockIteration);
-      Serial.print("ms): ");    
-      Serial.print("TDS Value: ");
-      Serial.print(TDS->tdsValue, 0);
-      Serial.print("ppm\t");
-      Serial.print("Voltage: ");
-      Serial.print(TDS->voltage, 2);
-      Serial.println("V");
-    }
-  }
-
-  // This might be helpful in getting a whole number of the amount of time that has passed during the measuring process that we can send to the app...
-  // If the programClock (ms) is over the first single second, increment the timeCount by another second...
-  if ( programClock > timeCount ) { 
-    timeCount += 1000;
-  }
-  delete TDS;
-  programClock = millis();  // programClock is initialized in Global scope. After iteration of each loop, get the current timepoint of this "sample" iteration...
+  fetchTDS();  // Get the TDS value and average voltage of that reading. Approximately 840ms (or a multiple) has to pass until a value is actually received, else equal 0.
+  
 }
 /*  ==================================================
     getMedianNum :: From DFRobot Basic Example...
@@ -132,7 +110,7 @@ float calculateTDS(float cVal) {
     Calculates the TDS reading from the meter/probe.
     Various conversions for the value, etc.
     ================================================== */
-void fetchTDS(TDSData * TDS) {
+void fetchTDS() {
   static unsigned long analogSampleTimepoint = millis();       // Get initial timepoint when we entered the loop (lookClock).
   if( millis() - analogSampleTimepoint > 40U ) {            // Every 40 milliseconds, read the analog value from the ADC. 40U is essentially the refresh rate of data fetching [Make it a variable?]
     analogSampleTimepoint = millis();                       // Assigning to a static variable? This value should be ~40ms after if-statement.
@@ -142,7 +120,7 @@ void fetchTDS(TDSData * TDS) {
   }
 
   static unsigned long printTimepoint = millis();             // Get initial timepoint (at this point, we know approximately that 40ms has passed).     
-  if( millis() - printTimepoint > 800U ) {                    // Every 800 milliseconds, convert the analog value from the ADC. 800U might be a variable that should be changed/dynamic.
+  if( millis() - printTimepoint > 800U ) {                    // Every 800 milliseconds, convert the analog value from the ADC. 800U might be a variable that should be changed/dynamic. Default: 800C, now a variable!
      printTimepoint = millis();                               // Assigning to a static variable? This value should be 840ms after if-statement.
      for( tempIndex = 0; tempIndex < SCOUNT; tempIndex++ ) { TDSBufferTemp[tempIndex] = TDSBuffer[tempIndex]; }
      avgV = getMedianNum(TDSBufferTemp, SCOUNT) * (float)VREF / 1024.0;           // See function description for use-case here...
@@ -150,8 +128,17 @@ void fetchTDS(TDSData * TDS) {
      float compensationVoltage = avgV / compensationCoefficient;                  // Temperature compensation.
      tdsVal = calculateTDS(compensationVoltage);
 
-     TDS->tdsValue = tdsVal;
-     TDS->voltage = avgV;
+    if( DEBUG ) {
+      //Serial.print("[ ");
+      //Serial.print(timeCount/1000);
+      //Serial.print("s ]:   ");   
+      Serial.print("TDS Value: ");
+      Serial.print(tdsVal, 0);
+      Serial.print("ppm   ");
+      Serial.print("Voltage: ");
+      Serial.print(avgV, 2);
+      Serial.println("V");
+    }
   }
   // At this point, approximately 840ms has passed. So, we are 260ms from reaching a full second cycle. This might be important in calculating refresh speeds.
 }
